f9b830870ba3ccb07274dba43f42abd0c9c22b17
zaki <zqbgds@163.com>
2020-11-16 14:28:59 +0800

    9.13 页导航栏的封装和使用

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    首页
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from "components/common/navbar/NavBar"
+  export default {
+    name: "Home",
+    components: {
+      NavBar
     }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
