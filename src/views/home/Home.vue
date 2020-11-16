32a7a001cecbbcf06d604a1031f6f7eb71136dcf
zaki <zqbgds@163.com>
2020-11-16 15:41:58 +0800

    9.14 请求首页的多个数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,10 +8,27 @@
 
 <script>
   import NavBar from "components/common/navbar/NavBar"
+  import { getHomeMultidata } from "network/home";
+
   export default {
     name: "Home",
     components: {
       NavBar
+    },
+    data(){
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      // 请求多个数据，因为后端提供的数据有时候都放在一起的
+      getHomeMultidata().then(res => {
+        // console.log(res)
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+
+      })
     }
   }
 </script>
