b99729be2125dc8523a0e7b30ce259459e6b7636
zaki <zqbgds@163.com>
2020-11-20 16:42:29 +0800

    10.13 完成上拉加载更多

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -14,6 +14,10 @@
       probeType: {
         type: Number,
         default: 0
+      },
+      pullUpLoad: {
+        type: Boolean,
+        default: false
       }
     },
     data(){
@@ -25,20 +29,27 @@
       this.scroll = new BScroll(this.$refs.wrapper, {
         // probeType: 3, // 值为3是性能最低的，所以我们封装的时候根据传入的值来设置
         probeType: this.probeType,
-        pullUpLoad: true,
+        // pullUpLoad: true, pullUpLoad也根据使用它的父组件来设置
+        pullUpLoad: this.pullUpLoad,
         click: true
       })
-
       // 监听滚动位置
       this.scroll.on('scroll', (position) => {
         // 通过$emit发送出去共外面的组件使用
         // console.log(position)
         this.$emit('scroll', position)
       })
+      // 监听上拉加载更多
+      this.scroll.on('pullingUp', () => {
+        this.$emit('pullingUp')
+      })
     },
     methods: {
       scrollTo(x, y, time = 500){
         return this.scroll.scrollTo(x, y, time = 500)
+      },
+      finishPullUp(){
+        this.scroll.finishPullUp()
       }
     }
   }
