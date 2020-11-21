3194dce095fd198b057890d8f58a523b33b2af4a
zaki <zqbgds@163.com>
2020-11-21 18:20:17 +0800

    11.9 跳转到详情页并携带iid

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="goods-item">
+  <div class="goods-item" @click="itemClick()">
     <img :src="goodsItem.show.img" alt="" @load="imageLoad">
     <div class="goods-info">
       <p>{{goodsItem.title}}</p>
@@ -24,6 +24,9 @@
       imageLoad(){
         // 发射事件总线
         this.$bus.$emit('itemImageLoad')
+      },
+      itemClick(){
+        this.$router.push('/detail/' + this.goodsItem.iid)
       }
     }
   }
