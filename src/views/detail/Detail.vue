2eb6a9ed7cb0f524d39e2f78d1017560adde91f7
zaki <zqbgds@163.com>
2020-11-21 19:32:02 +0800

    11.11 详情页数据请求及轮播图展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,40 @@
 <template>
-  <detail-nav-bar></detail-nav-bar>
+  <div id="detail">
+    <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :top-images="topImages"></detail-swiper>
+  </div>
+
 </template>
 
 <script>
   import DetailNavBar from "./childComps/DetailNavBar";
+  import DetailSwiper from "./childComps/DetailSwiper";
+
+  import { getDetail } from "network/detail";
 
   export default {
     name: "Detail",
     components: {
-      DetailNavBar
+      DetailNavBar,
+      DetailSwiper
     },
     data(){
       return {
-        iid: null
+        iid: null,
+        topImages: []
       }
     },
     created() {
-      this.iid = this.$route.params.iid
+      // 1.保存传入的iid
+      this.iid = this.$route.params.iid;
+      // 2.根据iid请求详情数据
+      // 同样的这里我们将网络请求封装到network中的detail.js
+      getDetail(this.iid).then(res => {
+        // 先获取顶部轮播图
+        this.topImages = res.result.itemInfo.topImages
+
+        console.log(res)
+      })
     }
   }
 </script>
---
+++ src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+  <swiper class="swiper-wrap">
+    <swiper-item v-for="item in topImages">
+      <img :src="item" alt="">
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import { Swiper, SwiperItem } from 'components/common/swiper'
+  export default {
+    name: "DetailSwiper",
+    components: {
+      Swiper,
+      SwiperItem
+    },
+    props: {
+      topImages: {
+        type: Array,
+        default(){
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .swiper-wrap{
+    /*这里如果将类名定义为swiper会和组件有冲突报错*/
+    height: 300px;
+    overflow: hidden;
+  }
+</style>
